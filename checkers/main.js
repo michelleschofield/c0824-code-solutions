4de232f181ed24a1caae1b54bd08daea2103e7e6
Michelle Schofield <schofieldshelly05@gmail.com>
2024-08-31 18:02:53 +0000

    Added functionality to canMoveWithoutTaking to allow kinged pieces to move how kinged pieces move

--- checkers/main.js
+++ checkers/main.js
@@ -94,71 +94,78 @@ function takePiece(location) {
     delete board[location[0]][location[1]].piece;
   }
 }
-function kingPiece(location) {
-  if (board[location[0]][location[1]].piece !== undefined) {
-    board[location[0]][location[1]].piece.kinged = true;
-  }
-}
+// // has to be commented out until I figure out how to fix the typescript error
+// function kingPiece(location: number[]): void {
+//   if (board[location[0]][location[1]].piece) {
+//     board[location[0]][location[1]].piece.kinged = true;
+//   }
+// }
 // lots and lots of tests
 function canMoveWithoutTaking(startLocation, endLocation) {
+  const piece = board[startLocation[0]][startLocation[1]].piece;
   if (
     !board[endLocation[0]][endLocation[1]].playable ||
     !board[startLocation[0]][startLocation[1]].playable
   ) {
     return false;
   } else if (
-    board[startLocation[0]][startLocation[1]].piece === undefined ||
+    piece === undefined ||
     board[endLocation[0]][endLocation[1]].piece !== undefined
   ) {
     return false;
   }
-  const color = board[startLocation[0]][startLocation[1]].piece?.color;
-  let direction;
-  if (color === 'red') {
-    direction = 1;
-  } else if (color === 'black') {
-    direction = -1;
-  } else {
-    console.log('ERROR: piece is neither black nor red');
-    return false;
-  }
-  const row = startLocation[0] + direction;
   const columns = [startLocation[1] + 1, startLocation[1] - 1];
-  if (endLocation[0] !== row) {
-    return false;
-  } else if (columns.includes(endLocation[1])) {
+  const rows = [];
+  if (piece.kinged) {
+    rows.push(startLocation[0] + 1, startLocation[0] - 1);
+  } else {
+    const color = piece.color;
+    let direction;
+    if (color === 'red') {
+      direction = 1;
+    } else if (color === 'black') {
+      direction = -1;
+    } else {
+      console.log('ERROR: piece is neither black nor red');
+      return false;
+    }
+    rows.push(startLocation[0] + direction);
+  }
+  if (rows.includes(endLocation[0]) && columns.includes(endLocation[1])) {
     return true;
   } else {
-    console.log('Unknown error in canMoveWithoutTaking()');
     return false;
   }
 }
-// work in progress
-function canMoveIfTaking(startLocation, endLocation) {
-  if (
-    startLocation[0] - endLocation[0] !== 2 &&
-    startLocation[0] - endLocation[0] !== -2
-  ) {
-    console.log('false because wrong row');
-    return false;
-  } else if (
-    startLocation[1] - endLocation[1] !== 2 &&
-    startLocation[1] - endLocation[1] !== -2
-  ) {
-    console.log('false because wrong column');
-    return false;
-  } else if (
-    board[startLocation[0]][startLocation[1]].piece === undefined ||
-    board[endLocation[0]][endLocation[1]].piece !== undefined
-  ) {
-    console.log('false because of empty start or full end');
-    return false;
-  } else {
-    const pieceTaking = board[startLocation[0]][startLocation[1]].piece;
-    const locationAttacked = [
-      startLocation[0] + (startLocation[0] - endLocation[0]),
-      startLocation[1] + (startLocation[1] - endLocation[1]),
-    ];
-    console.log('location attacked:', locationAttacked);
-  }
-}
+// // work in progress
+// function canMoveIfTaking(
+//   startLocation: number[],
+//   endLocation: number[]
+// ): boolean {
+//   if (
+//     startLocation[0] - endLocation[0] !== 2 &&
+//     startLocation[0] - endLocation[0] !== -2
+//   ) {
+//     console.log('false because wrong row');
+//     return false;
+//   } else if (
+//     startLocation[1] - endLocation[1] !== 2 &&
+//     startLocation[1] - endLocation[1] !== -2
+//   ) {
+//     console.log('false because wrong column');
+//     return false;
+//   } else if (
+//     board[startLocation[0]][startLocation[1]].piece === undefined ||
+//     board[endLocation[0]][endLocation[1]].piece !== undefined
+//   ) {
+//     console.log('false because of empty start or full end');
+//     return false;
+//   } else {
+//     const pieceTaking = board[startLocation[0]][startLocation[1]].piece;
+//     const locationAttacked = [
+//       startLocation[0] + (startLocation[0] - endLocation[0]),
+//       startLocation[1] + (startLocation[1] - endLocation[1]),
+//     ];
+//     console.log('location attacked:', locationAttacked);
+//   }
+// }
--- checkers/main.ts
+++ checkers/main.ts
@@ -106,46 +106,50 @@
 //   }
 // }
 
-// function kingPiece(location: number[]): void {
-//   if (board[location[0]][location[1]].piece !== undefined) {
-//     board[location[0]][location[1]].piece.kinged = true;
-//   }
-// }
+// // // has to be commented out until I figure out how to fix the typescript error
+// // function kingPiece(location: number[]): void {
+// //   if (board[location[0]][location[1]].piece) {
+// //     board[location[0]][location[1]].piece.kinged = true;
+// //   }
+// // }
 
 // // lots and lots of tests
 // function canMoveWithoutTaking(
 //   startLocation: number[],
 //   endLocation: number[]
 // ): boolean {
+//   const piece = board[startLocation[0]][startLocation[1]].piece;
 //   if (
 //     !board[endLocation[0]][endLocation[1]].playable ||
 //     !board[startLocation[0]][startLocation[1]].playable
 //   ) {
 //     return false;
 //   } else if (
-//     board[startLocation[0]][startLocation[1]].piece === undefined ||
+//     piece === undefined ||
 //     board[endLocation[0]][endLocation[1]].piece !== undefined
 //   ) {
 //     return false;
 //   }
-//   const color = board[startLocation[0]][startLocation[1]].piece?.color;
-//   let direction;
-//   if (color === 'red') {
-//     direction = 1;
-//   } else if (color === 'black') {
-//     direction = -1;
-//   } else {
-//     console.log('ERROR: piece is neither black nor red');
-//     return false;
-//   }
-//   const row = startLocation[0] + direction;
 //   const columns = [startLocation[1] + 1, startLocation[1] - 1];
-//   if (endLocation[0] !== row) {
-//     return false;
-//   } else if (columns.includes(endLocation[1])) {
+//   const rows: number[] = [];
+//   if (piece.kinged) {
+//     rows.push(startLocation[0] + 1, startLocation[0] - 1);
+//   } else {
+//     const color = piece.color;
+//     let direction;
+//     if (color === 'red') {
+//       direction = 1;
+//     } else if (color === 'black') {
+//       direction = -1;
+//     } else {
+//       console.log('ERROR: piece is neither black nor red');
+//       return false;
+//     }
+//     rows.push(startLocation[0] + direction);
+//   }
+//   if (rows.includes(endLocation[0]) && columns.includes(endLocation[1])) {
 //     return true;
 //   } else {
-//     console.log('Unknown error in canMoveWithoutTaking()');
 //     return false;
 //   }
 // }
