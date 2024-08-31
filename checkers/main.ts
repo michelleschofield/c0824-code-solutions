2323c61e1cd0496f86eea6c5bc36bdb859545ebe
Michelle Schofield <schofieldshelly05@gmail.com>
2024-08-31 16:36:27 +0000

    canMoveWithoutTaking seems to working!

--- checkers/main.ts
+++ checkers/main.ts
@@ -112,64 +112,73 @@
 //   }
 // }
 
-// // needs to be changed to test if color can move that way
+// // lots and lots of tests
 // function canMoveWithoutTaking(
 //   startLocation: number[],
 //   endLocation: number[]
 // ): boolean {
 //   if (
+//     !board[endLocation[0]][endLocation[1]].playable ||
+//     !board[startLocation[0]][startLocation[1]].playable
+//   ) {
+//     return false;
+//   } else if (
 //     board[startLocation[0]][startLocation[1]].piece === undefined ||
 //     board[endLocation[0]][endLocation[1]].piece !== undefined
 //   ) {
 //     return false;
-//   } else if (startLocation[0] === endLocation[0]) {
-//     return false;
-//   } else if (
-//     startLocation[1] - endLocation[1] > 1 ||
-//     startLocation[1] - endLocation[1] < -1
-//   ) {
-//     return false;
-//   } else if (
-//     startLocation[0] - endLocation[0] > 1 ||
-//     startLocation[0] - endLocation[0] < -1
-//   ) {
-//     return false;
-//   } else if (!board[endLocation[0]][endLocation[1]].playable) {
-//     return false;
+//   }
+//   const color = board[startLocation[0]][startLocation[1]].piece?.color;
+//   let direction;
+//   if (color === 'red') {
+//     direction = 1;
+//   } else if (color === 'black') {
+//     direction = -1;
 //   } else {
+//     console.log('ERROR: piece is neither black nor red');
+//     return false;
+//   }
+//   const row = startLocation[0] + direction;
+//   const columns = [startLocation[1] + 1, startLocation[1] - 1];
+//   if (endLocation[0] !== row) {
+//     return false;
+//   } else if (columns.includes(endLocation[1])) {
 //     return true;
+//   } else {
+//     console.log('Unknown error in canMoveWithoutTaking()');
+//     return false;
 //   }
 // }
 
-// // work in progress
-// function canMoveIfTaking(
-//   startLocation: number[],
-//   endLocation: number[]
-// ): boolean {
-//   if (
-//     startLocation[0] - endLocation[0] !== 2 &&
-//     startLocation[0] - endLocation[0] !== -2
-//   ) {
-//     console.log('false because wrong row');
-//     return false;
-//   } else if (
-//     startLocation[1] - endLocation[1] !== 2 &&
-//     startLocation[1] - endLocation[1] !== -2
-//   ) {
-//     console.log('false because wrong column');
-//     return false;
-//   } else if (
-//     board[startLocation[0]][startLocation[1]].piece === undefined ||
-//     board[endLocation[0]][endLocation[1]].piece !== undefined
-//   ) {
-//     console.log('false because of empty start or full end');
-//     return false;
-//   } else {
-//     const pieceTaking = board[startLocation[0]][startLocation[1]].piece;
-//     const locationAttacked = [
-//       startLocation[0] + (startLocation[0] - endLocation[0]),
-//       startLocation[1] + (startLocation[1] - endLocation[1]),
-//     ];
-//     console.log('location attacked:', locationAttacked);
-//   }
-// }
+// // // work in progress
+// // function canMoveIfTaking(
+// //   startLocation: number[],
+// //   endLocation: number[]
+// // ): boolean {
+// //   if (
+// //     startLocation[0] - endLocation[0] !== 2 &&
+// //     startLocation[0] - endLocation[0] !== -2
+// //   ) {
+// //     console.log('false because wrong row');
+// //     return false;
+// //   } else if (
+// //     startLocation[1] - endLocation[1] !== 2 &&
+// //     startLocation[1] - endLocation[1] !== -2
+// //   ) {
+// //     console.log('false because wrong column');
+// //     return false;
+// //   } else if (
+// //     board[startLocation[0]][startLocation[1]].piece === undefined ||
+// //     board[endLocation[0]][endLocation[1]].piece !== undefined
+// //   ) {
+// //     console.log('false because of empty start or full end');
+// //     return false;
+// //   } else {
+// //     const pieceTaking = board[startLocation[0]][startLocation[1]].piece;
+// //     const locationAttacked = [
+// //       startLocation[0] + (startLocation[0] - endLocation[0]),
+// //       startLocation[1] + (startLocation[1] - endLocation[1]),
+// //     ];
+// //     console.log('location attacked:', locationAttacked);
+// //   }
+// // }
