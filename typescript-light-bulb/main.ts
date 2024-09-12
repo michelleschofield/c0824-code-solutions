33f489d153b1b64ee709225d271bd6ccaffefceb
Michelle Schofield <schofieldshelly05@gmail.com>
2024-09-12 17:21:25 +0000

    Completed typescript-light-bulb challenge

---
+++ typescript-light-bulb/main.ts
@@ -0,0 +1,27 @@
+const $lightBulb = document.querySelector('.light-bulb');
+const $container = document.querySelector('.container');
+const $moon = document.querySelector('.moon');
+
+if (!$lightBulb) throw new Error('$lightBulb query failed');
+if (!$container) throw new Error('$container query failed');
+if (!$moon) throw new Error('$moon query failed');
+
+let time = 'day';
+
+function toggleDay(): void {
+  time = time === 'day' ? 'night' : 'day';
+  console.log('time:', time);
+}
+
+function updateStyles(): void {
+  if (!$lightBulb || !$container || !$moon)
+    throw new Error('$lightBulb or $container query failed');
+  $lightBulb.className = `light-bulb ${time}`;
+  $container.className = `container ${time}`;
+  $moon.className = `moon ${time}`;
+}
+
+$lightBulb.addEventListener('click', () => {
+  toggleDay();
+  updateStyles();
+});
