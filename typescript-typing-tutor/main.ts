50d8d5ab15d63214638ecee3a1baf364917c6a32
Michelle Schofield <schofieldshelly05@gmail.com>
2024-09-12 19:04:58 +0000

    Completed typescript-typing-tutor challenge

---
+++ typescript-typing-tutor/main.ts
@@ -0,0 +1,36 @@
+const $letters = document.querySelectorAll('.letter');
+const $accuracy = document.querySelector('.accuracy');
+
+let currentLetterIndex = 0;
+let missedLetters = 0;
+
+if (!$letters) throw new Error('$letters query failed');
+if (!$accuracy) throw new Error('$accuracy query failed');
+
+function handleKeyDown(event: KeyboardEvent): void {
+  const $currentLetter = $letters[currentLetterIndex];
+  if (
+    event.key === $currentLetter.textContent ||
+    ($currentLetter.textContent === '_' && event.key === ' ')
+  ) {
+    $currentLetter.className += ' completedLetter';
+    currentLetterIndex++;
+    const $nextLetter = $letters[currentLetterIndex];
+    if ($nextLetter) {
+      $nextLetter.className += ' currentLetter';
+    } else {
+      calculateAccuracy();
+    }
+  } else {
+    $currentLetter.className += ' missedLetter';
+    missedLetters++;
+  }
+}
+
+function calculateAccuracy(): void {
+  if (!$accuracy) throw new Error('$accuracy query failed');
+  const accuracy = (($letters.length - missedLetters) / $letters.length) * 100;
+  $accuracy.textContent = `Accuracy: ${accuracy}%`;
+}
+
+document.addEventListener('keydown', handleKeyDown);
