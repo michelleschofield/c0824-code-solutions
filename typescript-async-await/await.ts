901059e44955c65e20c27de837d7ea7ed8650140
Michelle Schofield <schofieldshelly05@gmail.com>
2024-09-27 21:10:29 +0000

    got rid of thens

--- typescript-async-await/await.ts
+++ typescript-async-await/await.ts
@@ -7,37 +7,28 @@ const elapsed = (): string =>
   `${Math.round((Date.now() - startTime) / 1000)}s -`;
 
 async function readOnce(): Promise<void> {
-  return await read('foo/bar.html').then((msg) =>
-    console.log(elapsed(), 'readOnce:', msg)
-  );
+  const msg = await read('foo/bar.html');
+  console.log(elapsed(), 'readOnce:', msg);
 }
 
 async function readSeveral(): Promise<void> {
-  return await read('foo1/bar.html')
-    .then(async (msg) => {
-      console.log(elapsed(), 'readSeveral1:', msg);
-      return await read('foo2/bar.html');
-    })
-    .then(async (msg) => {
-      console.log(elapsed(), 'readSeveral2:', msg);
-      return await read('foo3/bar.html');
-    })
-    .then(async (msg) => console.log(elapsed(), 'readSeveral3:', msg));
+  const msg1 = await read('foo1/bar.html');
+  console.log(elapsed(), 'readSeveral1:', msg1);
+  const msg2 = await read('foo2/bar.html');
+  console.log(elapsed(), 'readSeveral2:', msg2);
+  const msg3 = await read('foo3/bar.html');
+  console.log(elapsed(), 'readSeveral3:', msg3);
 }
 
 async function readChained(): Promise<void> {
-  return await read('foo-chain/bar.html')
-    .then(async (msg1) => {
-      console.log(elapsed(), 'readChained1:', msg1);
-      return await read(msg1);
-    })
-    .then(async (msg2) => {
-      console.log(elapsed(), 'readChained2:', msg2);
-      return await read(msg2);
-    })
-    .then(async (msg3) => console.log(elapsed(), 'readChained3:', msg3));
+  const msg1 = await read('foo-chain/bar.html');
+  console.log(elapsed(), 'readChained1:', msg1);
+  const msg2 = await read(msg1);
+  console.log(elapsed(), 'readChained2:', msg2);
+  const msg3 = await read(msg2);
+  console.log(elapsed(), 'readChained3:', msg3);
 }
 
-readOnce()
-  .then(() => readSeveral())
-  .then(() => readChained());
+await readOnce();
+await readSeveral();
+await readChained();
