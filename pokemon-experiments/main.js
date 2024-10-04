23acc60a7802a4006458fe36995bc05581dda341
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-04 20:40:42 +0000

    found some stuff and I like it

--- pokemon-experiments/main.js
+++ pokemon-experiments/main.js
@@ -1,16 +1,14 @@
-async function fetchPokemon(): Promise<void> {
+'use strict';
+async function fetchPokemon() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
-
     if (!response.ok) {
       throw new Error(`HTTP Error! Status: ${response.status}`);
     }
-
     const pokemon = await response.json();
     console.log('Pokemon:', pokemon);
   } catch (error) {
     console.log('Error:', error);
   }
 }
-
 fetchPokemon();
