4de3f566a12e5d5ea966f3350218f6ff153ba22d
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-15 00:06:05 +0000

    Completed react-components exercise

---
+++ react-components/space-app/src/ImageDescription.tsx
@@ -0,0 +1,19 @@
+export function ImageDescription() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet. Gas colorful stars gas cool void gas planets
+        super gas super beautiful gas darkness cool gas super darkness gas deep
+        colors. Gas space void gas super stars gas distant colorful gas super
+        deep gas deep planets gas planets stars.
+      </p>
+      <p>
+        Gas swirls super gas distant super gas deep swirls gas colors stars gas
+        space bright gas colors galaxy? Gas space darkness gas super swirls gas
+        super darkness gas deep cool gas cool stars gas swirls void gas void
+        swirls. Gas super void gas void swirls gas darkness void gas darkness
+        beautiful gas beautiful bright gas cool super gas deep colors.
+      </p>
+    </>
+  );
+}
