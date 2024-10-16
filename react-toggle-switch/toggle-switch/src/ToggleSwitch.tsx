b472403798cdb648214cb973e01f233d19586ee6
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-16 19:40:57 +0000

    Completed toggle-switch exercise

---
+++ react-toggle-switch/toggle-switch/src/ToggleSwitch.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export function ToggleSwitch() {
+  const [active, setActive] = useState(false);
+
+  function handleClick() {
+    setActive(!active);
+  }
+
+  return (
+    <div className="switch-container">
+      <div onClick={handleClick} className={`switch ${active ? 'on' : 'off'}`}>
+        <div className="circle"></div>
+      </div>
+      <p>On</p>
+    </div>
+  );
+}
