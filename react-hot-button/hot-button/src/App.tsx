706a074f30df5dec15b373ba0a3d9a3a89440c53
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-16 18:10:19 +0000

    Refactoring

--- react-hot-button/hot-button/src/App.tsx
+++ react-hot-button/hot-button/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
-import { HotButton } from './HotButton';
-import { CoolButton } from './CoolButton';
+
+import { TempChangingButton } from './TempChangingButton';
 
 export function App() {
   const [temp, setTemp] = useState(0);
@@ -14,23 +14,31 @@ export function App() {
     setTemp(temp - 1);
   }
 
+  function determineClass(temp: number): string {
+    if (temp < -9) return 'freezing';
+    else if (temp < -6) return 'cold';
+    else if (temp < -3) return 'cool';
+    else if (temp < 3) return 'normal';
+    else if (temp < 6) return 'warm';
+    else if (temp < 9) return 'hot';
+    else if (temp < 12) return 'really-hot';
+    else return 'blazing';
+  }
+
   const className = determineClass(temp);
 
   return (
     <>
-      <HotButton className={className} onIncreaseTemp={handleIncreaseTemp} />
-      <CoolButton className={className} onDecreaseTemp={handleDecreaseTemp} />
+      <TempChangingButton
+        className={className}
+        onChangeTemp={handleIncreaseTemp}
+        label="Heat Up"
+      />
+      <TempChangingButton
+        className={className}
+        onChangeTemp={handleDecreaseTemp}
+        label="Cool Down"
+      />
     </>
   );
-}
-
-function determineClass(temp: number): string {
-  if (temp < -9) return 'freezing';
-  else if (temp < -6) return 'cold';
-  else if (temp < -3) return 'cool';
-  else if (temp < 3) return 'normal';
-  else if (temp < 6) return 'warm';
-  else if (temp < 9) return 'hot';
-  else if (temp < 12) return 'really-hot';
-  else return 'blazing';
 }
