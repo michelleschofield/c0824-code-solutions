19029943140142032858250271c4c8a25b2ca6dd
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-18 22:30:16 +0000

    a message is displayed when there are no matching items

--- react-search/react-search/src/ItemList.tsx
+++ react-search/react-search/src/ItemList.tsx
@@ -4,14 +4,16 @@ type Props = {
 };
 
 export function ItemList({ items, filterText }: Props) {
-  return (
-    <ul>
-      {items.map((item) => {
-        if (item.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
-          return null;
-        }
-        return <li key={item}>{item}</li>;
-      })}
-    </ul>
+  const shownItems: JSX.Element[] = [];
+
+  items.forEach((item) => {
+    if (item.toLowerCase().indexOf(filterText.toLowerCase()) === -1) return;
+    shownItems.push(<li key={item}>{item}</li>);
+  });
+
+  return shownItems.length ? (
+    <ul>{shownItems}</ul>
+  ) : (
+    <p>No items match your search</p>
   );
 }
