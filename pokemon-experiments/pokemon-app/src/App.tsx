e0c7be46f69e6d00b901abdc79b0d2bd8744a0a8
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-21 18:11:40 +0000

    made more pokemon cards

--- pokemon-experiments/pokemon-app/src/App.tsx
+++ pokemon-experiments/pokemon-app/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css';
 import { PokeballCard } from './PokeballCard';
+import { PokemonCard } from './PokemonCard';
+import { TextCard } from './TextCard';
 
 function App() {
+  function formatFlavorText(text: string) {
+    return text.split('\n').join(' ').split('\f').join(' ');
+  }
+
   return (
     <>
       <PokeballCard />
+      <PokemonCard caption="Bulbasaur" imageSrc="bulbasaur.png" />
+      <TextCard
+        text={formatFlavorText(
+          'A strange seed was\nplanted on its\nback at birth.\fThe plant sprouts\nand grows with\nthis POKéMON.'
+        )}
+      />
     </>
   );
 }
