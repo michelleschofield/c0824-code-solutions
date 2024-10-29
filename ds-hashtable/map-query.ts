727fda595b0379e69d1d07f1355de065c2661a82
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-29 21:26:14 +0000

    sorts results by number of words in the documents

--- ds-hashtable/map-query.ts
+++ ds-hashtable/map-query.ts
@@ -1,20 +1,41 @@
 import { Document, DocumentIndex } from './map-index';
 
+type QueryResult = {
+  document: Document;
+  appearances: number;
+};
+
+type QueryResults = {
+  [index: string]: QueryResult;
+};
+
 /**
  * Queries a DocumentIndex and returns all the Documents that contain
  * any of the words in the query, as a Set.
  */
-export function queryIndex(index: DocumentIndex, query: string): Set<Document> {
+export function queryIndex(index: DocumentIndex, query: string): Document[] {
   const words = query.toLocaleLowerCase().match(/\b(\w+)\b/g);
-  const results = new Set<Document>();
+  const results: QueryResults = {};
 
   words?.forEach((word) => {
     const docs = index.get(word);
 
     docs?.forEach((doc) => {
-      results.add(doc);
+      const result = results[doc.title];
+
+      if (result) {
+        result.appearances++;
+      } else {
+        results[doc.title] = { document: doc, appearances: 1 };
+      }
     });
   });
 
-  return results;
+  const sortedResults = Object.values(results).sort(
+    (a, b) => b.appearances - a.appearances
+  );
+
+  const simplifiedResults = sortedResults.map((result) => result.document);
+
+  return simplifiedResults;
 }
