5292ea234a2aa83a649fdb011408f1b467308c55
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-29 16:18:31 +0000

    smallest works

--- ds-bst/bst-smallest.ts
+++ ds-bst/bst-smallest.ts
@@ -4,9 +4,17 @@ import { BinarySearchTree, TreeNode } from './lib/bst';
  * Returns the smallest value in the BST, or `Infinity` if the tree is empty.
  */
 export function smallest(bst: BinarySearchTree): number {
-  return 0;
+  return smallestRecursive(bst.root);
 }
 
 function smallestRecursive(node: TreeNode | undefined): number {
-  return 0;
+  if (!node) {
+    return Infinity;
+  }
+
+  if (!node.left) {
+    return node.value;
+  }
+
+  return smallestRecursive(node.left);
 }
