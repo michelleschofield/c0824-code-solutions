a31bff09b39c10aa0d95a6edc084e9fe810c3648
Michelle Schofield <schofieldshelly05@gmail.com>
2024-11-04 21:49:15 +0000

    get specific grade working

--- express-crud/server.ts
+++ express-crud/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import pg from 'pg';
-import { errorMiddleware } from './lib/index.js';
+import { ClientError, errorMiddleware } from './lib/index.js';
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
@@ -21,8 +21,27 @@ app.get('/api/grades', async (req, res, next) => {
     `;
     const result = await db.query(sql);
     const grades = result.rows;
-    if (!grades) res.sendStatus(500);
     res.status(200).json(grades);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/grades/:gradeId', async (req, res, next) => {
+  try {
+    const { gradeId } = req.params;
+    if (!Number.isInteger(+gradeId)) {
+      throw new ClientError(400, `${gradeId} is not an integer`);
+    }
+    const sql = `
+      select *
+      from "grades"
+      where "gradeId" = $1;
+    `;
+    const result = await db.query(sql, [gradeId]);
+    const grade = result.rows[0];
+    if (!grade) throw new ClientError(404, `grade ${gradeId} not found`);
+    res.status(200).json(grade);
   } catch (err) {
     next(err);
   }
