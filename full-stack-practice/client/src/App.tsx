4538100686206e0b5f4541b42fef36a3a1b93813
Michelle Schofield <schofieldshelly05@gmail.com>
2024-11-05 23:36:53 +0000

    added header

--- full-stack-practice/client/src/App.tsx
+++ full-stack-practice/client/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Header />}>
-        <Route index path="catalog" element={<Catalog />} />
+        <Route index element={<Catalog />} />
         <Route path="details/:productId" element={<Details />} />
         <Route path="about" element={<About />} />
         <Route path="*" element={<NotFound />} />
