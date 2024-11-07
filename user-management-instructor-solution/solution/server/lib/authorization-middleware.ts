673e8264e5a851c252ee9b719e0dfbd9da7b0237
Michelle Schofield <schofieldshelly05@gmail.com>
2024-11-07 17:21:08 +0000

    downloaded instructor solution

---
+++ user-management-instructor-solution/solution/server/lib/authorization-middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { ClientError } from './client-error.js';
+
+const hashKey = process.env.TOKEN_SECRET ?? '';
+if (!hashKey) throw new Error('TOKEN_SECRET not found in env');
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  // The token will be in the Authorization header with the format `Bearer ${token}`
+  const token = req.get('authorization')?.split('Bearer ')[1];
+  if (!token) throw new ClientError(401, 'No JWT token found');
+  const payload = jwt.verify(token, hashKey);
+  req.user = payload as Request['user'];
+  next();
+}
